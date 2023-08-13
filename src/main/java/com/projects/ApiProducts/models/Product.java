fe0f9c1598a1574f3aa9faccb0addf1bfbe66d7c
Yeison Estiven Perea Córdoba <yeisonestivenperea@gmail.com>
2023-08-12 23:43:37 -0500

    first commit

---
+++ src/main/java/com/projects/ApiProducts/models/Product.java
@@ -0,0 +1,42 @@
+package com.projects.ApiProducts.models;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Yeison Perea
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private float price;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = new Date();
+    }
+}
